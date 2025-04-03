d91a6aa9c6c6c98c14357b1556a290985f56d161
Natan Foleto <natanfoleto2015@hotmail.com>
2025-04-03 02:18:42 -0300

    added tailwind and shadcn on manager

--- apps/manager/src/App.tsx
+++ apps/manager/src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
   return (
-    <div>
-      <p style={{ fontFamily: 'Poppins' }}>Natan Foleto (Poppins)</p>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
     </div>
   )
 }
