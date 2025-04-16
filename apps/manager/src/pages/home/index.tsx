bbdc2030e70d8d2ae99fdaf3c8dd3a9633dc5168
Natan Foleto <natanfoleto2015@hotmail.com>
2025-04-16 16:39:09 -0300

    added create school on home page

---
+++ apps/manager/src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import { Plus } from 'lucide-react'
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { CreateSchool } from '@/pages/app/@sheet/schools/create-school'
+
+import { SchoolList } from './school-list'
+
+export function Home() {
+  const [createSchool, setCreateSchool] = useState(false)
+
+  function toggleCreateSchool() {
+    setCreateSchool(!createSchool)
+  }
+
+  return (
+    <div className="w-full space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-medium">Escolas</h1>
+
+        <Button className="cursor-pointer" onClick={toggleCreateSchool}>
+          <Plus className="size-4" />
+          Criar escola
+        </Button>
+      </div>
+
+      <SchoolList />
+
+      <CreateSchool open={createSchool} onClose={toggleCreateSchool} />
+    </div>
+  )
+}
